c4f101fdd4e503e25ebb50bcf8c06896c7989eb5
quaum <mdaq78@gmail.com>
2018-03-10 15:12:54 +0600

    lrCostFunction completed

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -39,6 +39,14 @@ grad = zeros(size(theta));
 
 
 
+h = (sigmoid(X * theta));
+
+
+J = -(1/m) * ( y' * log(h) +  (1-y)' *log(1-h) ) + (lambda/(2*m))* sum((theta(2:end)).^2);
+
+
+grad = (1/m) * ( X' * (h-y));
+grad(2:end) = grad(2:end) + (lambda/m)* (theta(2:end));
 
 
 
