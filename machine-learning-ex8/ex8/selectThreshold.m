bdb2ad79390bef9abf0f9d9186ace2f06f0622e4
MAQ678 <abdulquayum678@gmail.com>
2018-04-24 08:26:56 +0600

    Issues on 2nd task

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -12,7 +12,7 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
+    epsilon = 8.99e-05
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
@@ -24,8 +24,17 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
-
-
+  predictions = pval <epsilon;
+  
+  
+  tp = sum((predictions == 1) && (yval == 1));
+  fp = sum((predictions == 1) && (yval == 0));
+  fn = sum((predictions == 0) && (yval == 1));
+  
+  prec = tp / (tp+fp);
+  rec = tp / (tp+fn);
+  
+  F1 = 2* (prec * rec)/(prec + rec)
 
 
 
